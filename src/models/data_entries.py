3770138961eb6cc94c920fcd4ee277ee51dda8bd
IvakhaBohdan <chesteemiller22@gmail.com>
2025-11-18 18:22:44 +0200

    CTF-03 (P5): Клас харчування із загальними методами розрахунку

--- src/models/data_entries.py
+++ src/models/data_entries.py
@@ -13,3 +13,21 @@ class Product:
             'fats': self.fats,
             'carbs': self.carbs
         }
+    
+class Meal:
+    def __init__(self, meal_type: str, time: str, products: list):
+        self.meal_type = meal_type 
+        self.time = time
+        self.products = products  
+
+    def calculate_total_calories(self) -> float:
+        total_cal = sum(p.calories for p in self.products)
+        return total_cal
+
+    def calculate_total_nutrients(self) -> dict:
+        total = {'proteins': 0, 'fats': 0, 'carbs': 0}
+        for p in self.products:
+            total['proteins'] += p.proteins
+            total['fats'] += p.fats
+            total['carbs'] += p.carbs
+        return total
